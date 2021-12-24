aea72d96cadfe1b60924ba9657c6e213b495e143
hoanghailongvn <hoanghailong2642k@gmail.com>
2021-12-24 15:45:32 +0700

    first commit

---
+++ resources/js/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div className="container">
+           
+        </div>
+    );
+}
+
+export default App;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
+}
